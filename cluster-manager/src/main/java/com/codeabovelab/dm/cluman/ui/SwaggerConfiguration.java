a2cd6f4b781fa5be948688883dc2f15d912e7f92
pronto <vitaly.pronto@gmail.com>
2016-11-16 16:56:33 +0300

    added apiInfo to swagger

--- cluster-manager/src/main/java/com/codeabovelab/dm/cluman/ui/SwaggerConfiguration.java
+++ cluster-manager/src/main/java/com/codeabovelab/dm/cluman/ui/SwaggerConfiguration.java
@@ -59,7 +59,7 @@ public class SwaggerConfiguration {
         return new ApiInfoBuilder()
                 .title("DockMaster API")
                 .description("Dockmaster is an open source Docker container management system. " +
-                        "It integrates containers, applications, clusters, images, and registries management in one single place. ")
+                        "It integrates container, application, cluster, image, and registry management in one single place.")
                 .contact(new Contact("Code Above Lab", "https://github.com/codeabovelab/dockmaster-platform", "info@codeabovelab.com"))
                 .license("Apache License")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
