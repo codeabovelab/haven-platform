e32411accbd3892e5cb1b6b4349c5f07c8d89fe8
pronto <vitaly.pronto@gmail.com>
2016-11-22 23:42:16 +0300

    added @Component to properties

--- cluster-manager/src/main/java/com/codeabovelab/dm/cluman/configuration/DataLocatinConfiguration.java
+++ cluster-manager/src/main/java/com/codeabovelab/dm/cluman/configuration/DataLocatinConfiguration.java
@@ -18,10 +18,12 @@ package com.codeabovelab.dm.cluman.configuration;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Data location configuration
  */
+@Component
 @ConfigurationProperties("dm.data")
 @Data
 public class DataLocatinConfiguration {
