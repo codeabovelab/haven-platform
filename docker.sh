30abeffd4eac16007a08f463988c246ee0313226
vitaly <vitaly.pronto@gmail.com>
2016-12-08 00:36:29 +0400

    removed not used code

--- docker.sh
+++ docker.sh
@@ -1,38 +1,16 @@
 #!/bin/bash
 
-echo "List of modules for deploying: "  ${LIST_OF_MODULES}
-echo "Current module:  "  $2
 export DOCKER_HOST=unix:///run/docker.sock
 export DOCKER_REPO=ni1.codeabovelab.com
 
 function deploy
 {
-        echo "Try to deploy to docker"
-        cd target
-        docker build -t ${DOCKER_REPO}/$2:$3 .
+        echo "Try to deploy to docker: " $2
+        docker build -t ${DOCKER_REPO}/$2:$3 target
         docker tag ${DOCKER_REPO}/$2:$3 ${DOCKER_REPO}/$2:latest
         docker push ${DOCKER_REPO}/$2
-
 }
 
-if [ -z ${LIST_OF_MODULES} ]
-then
-    deploy $1 $2 $3
-    exit
-else
-
-    for i in $(echo ${LIST_OF_MODULES} | tr "," "\n")
-    do
-        echo "Compare modules: " $i $2
-            if [ "$i" = "$2" ]
-            then
-                deploy $1 $2 $3 
-                exit
-            fi
-    done
-
-    echo "Skip deploying to docker"
-    exit
-    
-fi
+deploy $1 $2 $3
+exit
 
