ed0ca022b04c9b845905b3afc9ac0089d68fd779
wayerr <radiofun@ya.ru>
2017-01-10 20:18:12 +0300

    SwarmInspectResponse fix warn

--- cluster-manager/src/main/java/com/codeabovelab/dm/cluman/cluster/docker/model/swarm/SwarmInspectResponse.java
+++ cluster-manager/src/main/java/com/codeabovelab/dm/cluman/cluster/docker/model/swarm/SwarmInspectResponse.java
@@ -18,11 +18,13 @@ package com.codeabovelab.dm.cluman.cluster.docker.model.swarm;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Result of 'GET /swarm'
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class SwarmInspectResponse extends Cluster {
 
     @JsonProperty("JoinTokens")
