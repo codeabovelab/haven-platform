1d08ef420b5f1ef9fff1829a91552da2babfb366
pronto <vitaly.pronto@gmail.com>
2017-02-17 15:07:35 +0300

    added Removing env variable and labels inherited from image
    (cherry picked from commit 8e14576)

--- cluster-manager/src/main/java/com/codeabovelab/dm/cluman/batch/ContainerConfigTasklet.java
+++ cluster-manager/src/main/java/com/codeabovelab/dm/cluman/batch/ContainerConfigTasklet.java
@@ -20,12 +20,19 @@ import com.codeabovelab.dm.cluman.cluster.docker.management.DockerService;
 import com.codeabovelab.dm.cluman.cluster.docker.model.ContainerDetails;
 import com.codeabovelab.dm.cluman.job.JobComponent;
 import com.codeabovelab.dm.cluman.model.ContainerSource;
+import com.codeabovelab.dm.cluman.model.ImageDescriptor;
 import com.codeabovelab.dm.cluman.source.ContainerSourceFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Add current container arguments to processed container
  */
+@Slf4j
 @JobComponent
 public class ContainerConfigTasklet {
 
@@ -40,7 +47,34 @@ public class ContainerConfigTasklet {
         ContainerDetails container = service.getContainer(id);
         ContainerSource arg = new ContainerSource();
         containerSourceFactory.toSource(container, arg);
+        cleanArg(arg, container.getImageId());
         arg.setCluster(pc.getCluster());
         return pc.makeNew().src(arg).build();
+    }
+
+    /**
+     * Remove env variable and labels inherited from image
+     *
+     * @param arg
+     * @param imageId
+     */
+    private void cleanArg(ContainerSource arg, String imageId) {
+        try {
+            ImageDescriptor image = service.getImage(imageId);
+            if (image != null) {
+
+                Set<String> labels = image.getContainerConfig().getLabels().keySet();
+                if (!CollectionUtils.isEmpty(labels)) {
+                    arg.getLabels().keySet().removeAll(labels);
+                }
+
+                List<String> envs = image.getContainerConfig().getEnv();
+                if (!CollectionUtils.isEmpty(envs)) {
+                    arg.getEnvironment().removeAll(image.getContainerConfig().getEnv());
+                }
+            }
+        } catch (Exception e) {
+            log.error("can't clean arg", e);
+        }
     }
 }
