5dde8ca1d978db1aa014aae3738973b6a1fb8878
pronto <vitaly.pronto@gmail.com>
2017-09-24 17:07:38 +0300

    added @Validated to GitSettings

--- cluster-manager/src/main/java/com/codeabovelab/dm/cluman/configs/container/GitSettings.java
+++ cluster-manager/src/main/java/com/codeabovelab/dm/cluman/configs/container/GitSettings.java
@@ -20,9 +20,11 @@ import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @Data
+@Validated
 @ConfigurationProperties("dm.image.configuration.git")
 public class GitSettings {
 
